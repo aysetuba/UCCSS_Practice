b0cc69c3156cfe307f86276e9c8eff4e1fd6f4d4
Ayse Tuba <tubavardan@gmail.com>
2018-10-28 21:07:35 -0500

    nodemon install

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,5 @@
 // var express = require('express');
-
 // var app = express();
-
 
 // // CALLBACK FUNCTION
 // //response object and send you a message
@@ -11,8 +9,9 @@
 
 // app.listen(3300);
 
-// var express = require('express');
 
+//--------------------------------------------------------
+// var express = require('express');
 // var app = express();
 
 // app.set('port', process.env.PORT || 3000);
@@ -25,33 +24,45 @@
 // 	console.log('Express started on http://localhost:' + app.get('port'));
 // });
 
+////----------------------------------------------------------------------
 
 var http = require('http');
 var express = require('express');
 
 var app = express();
 
-app.use(function(req, res){
+app.use(function(req, res, next){
   console.log('Request from ' + req.ip);
+  next();
 });
 
 app.get('/',function(req,res){
 	res.send('Hello World!');
 });
 
+app.get('/about', function(req, res){
+	console.log('about');
+	res.send('About Us!');
+});
+
+app.get('/about/directions', function(req, res){
+	res.send('How to Find Us!');
+});
+
+
 app.use(function(req, res){
 	res.type('text/plan');
 	res.status(404);
-	res.send('404 Not Found');
+	res.send('Your page was not found!');
 });
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.type('text/plan');
 	res.status(500);
-	res.send('500 Sever Error');
+	res.send('500 Sever Error/ App Crash Error ');
 });
 
 http.createServer(app).listen(3000, function(){
 	console.log('Express server listening on port ' + 3000);
-});
+})

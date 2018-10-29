898f0f07d4cbaf808825620b8e36fd656032ced0
Ayse Tuba <tubavardan@gmail.com>
2018-10-28 22:15:55 -0500

    Gruntfile + ExpressJS is completed

--- Server/index.js
+++ Server/index.js
@@ -26,43 +26,56 @@
 
 ////----------------------------------------------------------------------
 
-var http = require('http');
-var express = require('express');
+// var http = require('http');
+// var express = require('express');
+// var path = require('path');
 
+
+// var app = express();
+// app.use(express.static(path.normalize(__dirname) + '/public'));
+
+// app.use(function(req, res, next){
+//   console.log('Request from ' + req.ip);
+//   next();
+// });
+
+// app.get('/',function(req,res){
+// 	res.send('Hello World!');
+// });
+
+// app.get('/about', function(req, res){
+// 	console.log('about');
+// 	res.send('About Us!');
+// });
+
+// app.get('/about/directions', function(req, res){
+// 	res.send('How to Find Us!');
+// });
+
+
+// app.use(function(req, res){
+// 	res.type('text/plan');
+// 	res.status(404);
+// 	res.send('Your page was not found!');
+// });
+
+// app.use(function(err, req, res, next){
+// 	console.error(err.stack);
+// 	res.type('text/plan');
+// 	res.status(500);
+// 	res.send('500 Sever Error/ App Crash Error ');
+// });
+
+// http.createServer(app).listen(3000, function(){
+// 	console.log('Express server listening on port ' + 3000);
+// })
+//--------------------------------------------------------------
+
+var express = require('express');
+var config = require('./config/config');
 var app = express();
 
-app.use(function(req, res, next){
-  console.log('Request from ' + req.ip);
-  next();
+require('./config/express')(app, config);
+require('http').createServer(app).listen(config.port, function () {
+	console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
-
-app.get('/',function(req,res){
-	res.send('Hello World!');
-});
-
-app.get('/about', function(req, res){
-	console.log('about');
-	res.send('About Us!');
-});
-
-app.get('/about/directions', function(req, res){
-	res.send('How to Find Us!');
-});
-
-
-app.use(function(req, res){
-	res.type('text/plan');
-	res.status(404);
-	res.send('Your page was not found!');
-});
-
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('text/plan');
-	res.status(500);
-	res.send('500 Sever Error/ App Crash Error ');
-});
-
-http.createServer(app).listen(3000, function(){
-	console.log('Express server listening on port ' + 3000);
-})
